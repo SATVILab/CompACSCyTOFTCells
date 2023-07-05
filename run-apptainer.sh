668306d3faa7933d34318cc829c2542578403543
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-05 16:23:19 +0100

    Improve apptainer initialisation
    - Make it able to start from any working directory
    - Make GITHUB_PAT equal to GH_TOKEN if it isn't set

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-test -e sif/r430.sif || scripts/ubuntu/download_apptainer.sh
+test -e ../Comp*/sif/r430.sif || ../Comp*/scripts/ubuntu/download_apptainer.sh
 R_LIBS_USER=$HOME/.local/.cache/R
 mkdir -p $R_LIBS_USER
-apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif ${1:-code-insiders tunnel --accept-server-license-terms}
+apptainer run --env R_LIBS=$R_LIBS_USER,RENV_CONFIG_PAK_ENABLED=TRUE,GITHUB_PAT=${GITHUB_PAT:=$GH_TOKEN} ../Comp*/sif/r430.sif ${1:-code-insiders tunnel --accept-server-license-terms}
--- scripts/ubuntu/download_apptainer.sh
+++ scripts/ubuntu/download_apptainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FETCH_R_VERSION=430
 GITHUB_OAUTH_TOKEN=$GH_TOKEN
-mkdir -p sif 
-./bin/fetch --repo="https://github.com/SATVILab/Comp23RodoSTA2005S" --tag="r${FETCH_R_VERSION}" --release-asset="r${FETCH_R_VERSION}.sif" --github-oauth-token="$GITHUB_OAUTH_TOKEN" sif
+mkdir -p ../Comp*/sif 
+../Comp*/bin/fetch --repo="https://github.com/SATVILab/Comp23RodoSTA2005S" --tag="r${FETCH_R_VERSION}" --release-asset="r${FETCH_R_VERSION}.sif" --github-oauth-token="$GITHUB_OAUTH_TOKEN" ../Comp*/sif
