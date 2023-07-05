f7f9dffa265236b65afa13212c07b1a8a802280c
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-05 14:19:14 +0000

    Use ~/.local/.cache instead of ~/.local/cache

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 test -e sif/r430.sif || scripts/ubuntu/download_apptainer.sh
-R_LIBS_USER=$HOME/.local/cache/R
+R_LIBS_USER=$HOME/.local/.cache/R
 mkdir -p $R_LIBS_USER
 apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif ${1:-code-insiders tunnel --accept-server-license-terms}
