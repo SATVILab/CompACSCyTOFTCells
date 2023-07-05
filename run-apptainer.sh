cc94caa1b441851f8ecb4c4511af212daad16925
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-05 16:05:34 +0200

    Change run-apptainer to download sif file if need be

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apptainer run sif/r423.sif code-insiders tunnel --accept-server-license-terms 
+test -e sif/r430.sif || scripts/ubuntu/download_apptainer.sh
+R_LIBS_USER=$HOME/.local/cache/R
+mkdir -p $R_LIBS_USER
+apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif code-insiders tunnel --accept-server-license-terms
