91d65865cf6c3f360ea1dbc9b517bf32e20e5cb4
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-05 16:09:40 +0200

    Allow using non-standard commands

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -2,4 +2,5 @@
 test -e sif/r430.sif || scripts/ubuntu/download_apptainer.sh
 R_LIBS_USER=$HOME/.local/cache/R
 mkdir -p $R_LIBS_USER
-apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif code-insiders tunnel --accept-server-license-terms
+cmd="${1:=code-insiders tunnel --accept-server-license-terms}"
+apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif $cmd
