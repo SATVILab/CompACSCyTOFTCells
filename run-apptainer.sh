8e4c1e1f25c05ec0877c40e1e09bd1f521c540ee
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-05 14:16:51 +0000

    Refine run apptainer command
    Can use :- to give default values when
    it's a positional parameter
    (as opposed to := when it's a regular
    parameter).

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -2,5 +2,4 @@
 test -e sif/r430.sif || scripts/ubuntu/download_apptainer.sh
 R_LIBS_USER=$HOME/.local/cache/R
 mkdir -p $R_LIBS_USER
-cmd=$1
-apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif ${cmd:=code-insiders tunnel --accept-server-license-terms}
+apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif ${1:-code-insiders tunnel --accept-server-license-terms}
