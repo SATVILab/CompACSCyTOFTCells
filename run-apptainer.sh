acd27b508ac47e9f7564a8cc819bb7f9160cb703
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-05 14:15:08 +0000

    Correct script to start apptainer

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -2,5 +2,5 @@
 test -e sif/r430.sif || scripts/ubuntu/download_apptainer.sh
 R_LIBS_USER=$HOME/.local/cache/R
 mkdir -p $R_LIBS_USER
-cmd="${1:=code-insiders tunnel --accept-server-license-terms}"
-apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif $cmd
+cmd=$1
+apptainer run --env R_LIBS=$R_LIBS_USER sif/r430.sif ${cmd:=code-insiders tunnel --accept-server-license-terms}
