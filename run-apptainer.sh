c8bf25f50ee635d6d568970213b2e66e6c583c2b
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-06 19:55:46 +0200

    Improve to correctly download to Comp directory

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-test -e ../Comp*/sif/r430.sif || ../Comp*/scripts/ubuntu/download_apptainer.sh
+comp_dir=$(ls .. | grep -e "^Comp")
+test -e ../"$comp_dir"/sif/r430.sif || ../"$comp_dir"/scripts/ubuntu/download_apptainer.sh
 R_LIBS_USER=$HOME/.local/.cache/R
 mkdir -p $R_LIBS_USER
-apptainer run --env R_LIBS=$R_LIBS_USER,RENV_CONFIG_PAK_ENABLED=TRUE,GITHUB_PAT=${GITHUB_PAT:=$GH_TOKEN} ../Comp*/sif/r430.sif ${1:-code-insiders tunnel --accept-server-license-terms}
+apptainer run --env R_LIBS=$R_LIBS_USER,RENV_CONFIG_PAK_ENABLED=TRUE,GITHUB_PAT=${GITHUB_PAT:=$GH_TOKEN} ../"$comp_dir"/sif/r430.sif ${1:-code-insiders tunnel --accept-server-license-terms}
--- scripts/ubuntu/download_apptainer.sh
+++ scripts/ubuntu/download_apptainer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 FETCH_R_VERSION=430
 GITHUB_OAUTH_TOKEN=$GH_TOKEN
-mkdir -p ../Comp*/sif 
-../Comp*/bin/fetch --repo="https://github.com/SATVILab/Comp23RodoSTA2005S" --tag="r${FETCH_R_VERSION}" --release-asset="r${FETCH_R_VERSION}.sif" --github-oauth-token="$GITHUB_OAUTH_TOKEN" ../Comp*/sif
+comp_dir=$(ls .. | grep -E "^Comp")
+mkdir -p ../"$comp_dir"/sif 
+../"$comp_dir"/bin/fetch --repo="https://github.com/SATVILab/$comp_dir" --tag="r${FETCH_R_VERSION}" --release-asset="r${FETCH_R_VERSION}.sif" --github-oauth-token="$GITHUB_OAUTH_TOKEN" ../"$comp_dir"/sif
+
