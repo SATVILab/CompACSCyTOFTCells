0f8d02530e982fa7b92f429e5201833788014152
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-06 21:14:10 +0100

    Improve apptainer initialisation
    - Enable using apptainer or singularity
      - Check which exists
      - Don't rely on `env` parameter

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+# get the Comp directory's full name.
+# needed as Comp* is not always expanded when 
+# needed beloww
 comp_dir=$(ls .. | grep -e "^Comp")
 test -e ../"$comp_dir"/sif/r423.sif || ../"$comp_dir"/scripts/ubuntu/download_apptainer.sh
-R_LIBS_USER=$HOME/.local/.cache/R
-mkdir -p $R_LIBS_USER
-apptainer run --env R_LIBS=$R_LIBS_USER,RENV_CONFIG_PAK_ENABLED=TRUE,GITHUB_PAT=${GITHUB_PAT:=$GH_TOKEN} ../"$comp_dir"/sif/r423.sif ${1:-code-insiders tunnel --accept-server-license-terms}
+# set R_LIBS if not set as otherwise
+# it is by default set to a non-writeable 
+# directory in the image
+R_LIBS=${R_LIBS:=$HOME/.local/.cache/R}
+# need to create the directory, as otherwise R_LIBS
+# will have no effect
+mkdir -p $R_LIBS
+RENV_CONFIG_PAK_ENABLED=TRUE
+GITHUB_PAT=${GITHUB_PAT:=$GH_TOKEN} 
+if [ which apptainer ]; then
+  container_runtime=apptainer
+elif [ which singularity ]; then
+  container_runtime=singularity
+else
+  echo "Neither singularity nor apptainer container runtime detected"
+  exit 1
+fi
+
+$containter_runtime run ../"$comp_dir"/sif/r423.sif ${1:-code-insiders tunnel --accept-server-license-terms}
