179b399838c5cfb9976c5611b53878d872be5361
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-13 11:59:41 +0100

    Debug config_r.sh

--- scripts/all/config_r.sh
+++ scripts/all/config_r.sh
@@ -1,12 +1,12 @@
 #!|usr/bin/env bash
 # github token
-if [ -n $GH_TOKEN ]; then 
-  export GITHUB_PAT=${GITHUB_PAT:-$GH_TOKEN}
-  export GITHUB_TOKEN=${GITHUB_PAT:-$GH_TOKEN}
+if [ -n "$GH_TOKEN" ]; then 
+  export GITHUB_PAT="${GITHUB_PAT:-"$GH_TOKEN"}"
+  export GITHUB_TOKEN="${GITHUB_PAT:-"$GH_TOKEN"}"
 fi
 
 # save all R packages to workspace if on GitPod
-if [ -n "$(env | grep -E "^GITPOD")"]; then
+if [ -n "$(env | grep -E "^GITPOD")" ]; then
     R_LIBS=${R_LIBS:=/"/workspace/.local/lib/R"}
     RENV_PATHS_CACHE=${RENV_PATHS_CACHE:=/workspace/.local/R/lib/renv}
     RENV_PATHS_LIBRARY_ROOT=${RENV_PATHS_LIBRARY_ROOT:=/workspace/.local/.cache/R/renv}
@@ -17,9 +17,9 @@ fi
 
 # ensure that radian works (at least on ephemeral dev
 # environments)
-if [ -n "$(env | grep -E "^GITPOD|^CODESPACE")"]; then
+if [ -n "$(env | grep -E "^GITPOD|^CODESPACE")" ]; then
   if ! [ -e "$HOME/.radian_profile" ]; then touch "$HOME/.radian_profile"; fi
-  if [ -z $(cat "$HOME/.radian_profile" | grep -E 'options(\s*radian\.editing_mode') ]; then 
+  if [ -z "$(cat "$HOME/.radian_profile" | grep -E 'options\(\s*radian\.editing_mode')" ]; then 
     echo 'options(radian.editing_mode = "vi")' >> "$HOME/.radian_profile"
   fi
 fi
